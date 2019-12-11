381412d70669f81f46d33f575741fdbb629a7491
fernando720 <fhac@cin.ufpe.br>
2019-12-11 00:42:08 -0300

    nada testado/feito no bloco de notas

--- EE2-fhac.py
+++ EE2-fhac.py
@@ -7,20 +7,23 @@ listaLinhas = arquivo.readlines()
 #criando classe veiculos com os atributos necessários para fazer a questão:
 class Veiculo:
     def __init__(self,infos):
-        listaInfo = infos.split()
-        self.tipo = listaInfo[0]
-        self.modelo = listaInfo[2]
-        self.ano = listaInfo[5]
-        self.autonomia = listaInfo[4]
-        self.reservado = listaInfo[9]
+        listaInfo = infos.split() 		#ao criar uma lista de objetos, foi necessário fazer o 'split()'. Cada item desta lista é um atributo distinto
+        self.tipo = listaInfo[0] 		#atributo 'tipo' é o primeiro item da lista retornada pelo 'split'
+        self.modelo = listaInfo[2] 		#atributo 'modelo' é o terceiro item da lista retornada pelo 'split()'
+        self.ano = listaInfo[5] 		#atributo 'ano' é o sexto item da lista retornada pelo 'split()'
+        self.autonomia = listaInfo[4] 		#atributo 'autonomia' é o quinto item da lista retornada pelo 'split()'
+        self.reservado = listaInfo[9] 		#atributo 'reservado' é o décimo item da lista retornada pelo 'split()'
     
 listaVeiculos =[]
 for n in range(1,len(listaLinhas)):
-    listaVeiculos.append(Veiculo(listaLinhas[n]))
+    listaVeiculos.append(Veiculo(listaLinhas[n])) #cria lista de objetos (veiculos) com os atributos da classe Veículo (tipo, modelo, ano, autonomia, reservado).
 
-#quantidade de carros, vans e utilitários:
+'''Descrição do passo acima: cada linha do documento 'veiculos.txt' vai ser recebida pela classe Veiculo, sofrerá o split(), ganhará os atributos da classe e será
+adicionada a listaVeiculos'''
+
+#letra a
 def tipos():
-    carros = 0
+    carros = 0 #iniciando contadores
     utes = 0
     vans = 0
     for vec in listaVeiculos:
@@ -31,7 +34,7 @@ def tipos():
         elif vec.tipo == "van":
             vans = vans + 1
 
-    tipos = ["carro","utilitário","van"]
+    tipos = ["carro","utilitário","van"] #listas que darão origem ao gráfico
     quantidade = [carros,utes,vans]
     
     plt.figure(1)
@@ -39,25 +42,75 @@ def tipos():
     plt.ylabel("Quantidade")
     plt.title("Quantidades de cada tipo de veículo")
 
+#letra b
 def reservados():
     
     listaModelos =[]
-    for vec in range(1,len(listaVeiculos)):
-        if listaVeiculos[vec].reservado == "True":
-            listaModelos.append(listaVeiculos[vec].modelo)
-    dictModelos = Counter(listaModelos)
+    for vec in range(0,len(listaVeiculos)):
+        if listaVeiculos[vec].reservado == "True": 		#se o veículo estiver reservado
+            listaModelos.append(listaVeiculos[vec].modelo) 	#adiciona o modelo deste veículo (que é um objeto com atributos, dentre eles, o modelo) à lista de modelos
+    dictModelos = Counter(listaModelos) 			'''faz a contagem de quanto cada modelo aparece e retorna uma lista de dicionários. Em cada um, 
+													a chave é o modelo e o valor da chave é a frequencia'''
     
     
-    modelos = dictModelos.keys()
-    quantidades = dictModelos.values()
-    print(dictModelos)
+    modelos = dictModelos.keys() 				#pega as chaves (modelo) da lista do Counter e adiciona em uma lista separada
+    quantidades = dictModelos.values() 			#pega os valores (frequencia com a qual cada modelo aparece) de cada chave e adiciona em outra lista separada
+    #print(dictModelos)
     
 
     plt.figure(2)
     plt.bar(modelos,quantidades)
     plt.ylabel("Quantidade")
     plt.title("Quantidade de veículos reservados por modelo")
+
+#Daqui para baixo, o código ainda não foi baixado (criado no bloco de notas por motivo de equipamento deficiente)
+
+#letra c
+def carros2017():
+    listaAno = []
+    for vec in range(0,len(listaVeiculos)):
+        listaAno.append(listaVeiculos[vec].ano) 	#segue o mesmo raciocínio da função anterior (reservado())
+    dictAno = Counter(listaAno)
+    
+    anosFabricacao = dictAno.keys()
+    quantidades = dictAno.values()
+    
+    plt.figure(3)
+    plt.bar(anosFabricacao,quantidades)
+    plt.ylabel("Quantidade de veículos")
+    plt.title("Quantidade de veículos por ano de fabricação")
+
+#letra d
+def mediaAutonomiaPorAno():
+    
+    listaAno = []
+    for vec in range(0,len(listaVeiculos)):
+	listaAno.append(listaVeiculos[vec].ano)
+    Contagem = Counter(listaAno) 			#contagem de cada ano
+    anosOrd = sorted(dictAno.items()) 			#retorna uma lista de tuplas ordenadas pelas chave (ano) do Counter(listaAno)
+    dictAno = dict(anosOrd) 				#Transforma lista de tuplas em lista de dicionários
+    anosFabricacao = dictAno.keys() 			#transforma as chaves (anos) em uma lista
+    frequenciaAno = dictAno.values() 			#transforma os valores das chaves (frequencia com a qual o ano aparece) em uma lista
+    autonomiaPorAno = []
+    for ano in dictAno:
+	somaAutonomia = 0
+	media = 0
+	for vec in range(0,len(listaVeiculos)):
+	    if anosFabricacao[ano] == listaVeiculos[vec].ano:
+		somaAutonomia = somaAutonomia + listaVeiculos[vec].autonomia
+	media = somaAutonomia/frequenciaAno[ano]
+	autonomiaPorAno.append(media)
+
+    '''A ordenação do dicionário foi importante para organizar o gráfico por ordem crescente de anos de fabricação'''
+
+    plt.figure(4)
+    plt.bar(anosFabricacao,autonomiaPorAno)
+    plt.ylabel("autonomia média (km/L)(?)")
+    plt.title("Autonomia média por ano de fabricação")
+
 tipos()
 reservados()
+carros2017()
+mediaAutonomiaPorAno()
 plt.show()
 
